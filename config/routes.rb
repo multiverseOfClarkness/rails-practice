50af9224e7935cad6abd504f56f0f9a9ae772950
multiverseOfClarkness <ladoboy92@gmail.com>
2023-03-24 10:37:36 +0800

    Comments feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,10 @@
 Rails.application.routes.draw do
   root "articles#index"
 
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
 end
 
 
-Rails.application.routes.draw do
-  get "/articles", to: "articles#index"
 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
-
-
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-end
