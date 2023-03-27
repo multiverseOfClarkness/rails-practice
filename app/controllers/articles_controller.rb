2207e4ec44350d0e2a9ed1f147b7ac05d64248a7
multiverseOfClarkness <ladoboy92@gmail.com>
2023-03-27 08:47:22 +0800

    Basic authentication

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+
+  http_basic_authenticate_with name: "clark", password: "clark", except: [:index, :show]
+
   def index
     @articles = Article.all
   end
